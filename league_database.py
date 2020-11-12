4f446cf1cfaa5f410e17500bc5b179bff6a16b16
Moch <moch.rene@googlemail.com>
2020-11-12 16:57:12 +0100

    match model added

--- league_database.py
+++ league_database.py
@@ -2,7 +2,7 @@ import pandas as pd
 import league_api as api
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
@@ -12,6 +12,7 @@ class Match(Base):
 
     platform_id = Column(String, primary_key=True)
     game_id = Column(String, primary_key=True)
+
     game_creation = Column(DateTime)
     game_duration = Column(Integer)
     queue_id = Column(String)
@@ -20,3 +21,33 @@ class Match(Base):
     game_version = Column(String)
     game_mode = Column(String)
     game_type = Column(String)
+
+
+class Team(Base):
+    __tablename__ = 'teams'
+
+    game_id = Column(String, primary_key=True)
+    team_id = Column(String, primary_key=True)
+
+    win = Column(String)
+    first_blood = Column(Boolean)
+    first_tower = Column(Boolean)
+    first_inhibitor = Column(Boolean)
+    first_baron = Column(Boolean)
+    first_dragon = Column(Boolean)
+    first_rift_herald = Column(Boolean)
+    tower_kills = Column(Integer)
+    inhibitor_kills = Column(Integer)
+    baron_kills = Column(Integer)
+    dragon_kills = Column(Integer)
+    vilemaw_kills = Column(Integer)
+    rift_herald_kills = Column(Integer)
+    dominion_victory_score = Column(Integer)
+
+
+
+
+
+def create_db_layout(connection_string: str):
+    engine = create_engine(connection_string, echo=True)
+    Base.metadata.create_all(engine)
